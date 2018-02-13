7f667594b0c0295b90773444627e2a02e6728e0a
Oriol Orra <oriolorraserra@gmail.com>
2018-02-13 18:20:49 +0100

    new mysql test project

---
+++ DB_test/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: oorra
+ *
+ * Created on February 13, 2018, 6:17 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
